8a8a415c4f759b4bfb70eff75c19363149ce882c
Ryan Barnes <rpbarnes9@gmail.com>
2017-04-08 18:02:18 -0700

    setting up node server with react components.

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+
+  module: {
+      loaders: [
+      {
+          test: /\.jsx?/,
+          include: APP_DIR,
+          loader: 'babel-loader'
+      }, {
+          test: /\.css/,
+          loader: 'style-loader'
+      }, {
+          test: /\.css/,
+          loader: 'css-loader',
+          query: {
+              modules: true,
+              localIndentName: '[name]__[local]__[hash:base64:5]'
+          }
+      }, {
+          test: /\.less/,
+          loader: 'less-loader'
+      }
+      ]
+  }
+};
+
+module.exports = config;
