af814453248967344d29dc1bbc05af3674016973
Ryan Barnes <rpbarnes9@gmail.com>
2017-04-10 21:51:09 -0700

    I added a component to add new cards. This is functional except that the done button does not behave properly. The first time it's rendered it doesn't fire. Then the second time it fires fine...

--- src/client/app/SectionContainer.jsx
+++ src/client/app/SectionContainer.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import DataEntryModal from './DataEntryModal.jsx';
+import CardDisplay from './CardDisplay.jsx';
+import AddCard from './AddCard.jsx';
 
 class SectionContainer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             title:'Section (Change)',
-            primaryText: '',
+            primaryText: 'This is new.',
             secondaryText: '',
-            modalOpen: false
+            modalOpen: false,
+            cards: []
         };
         this.handleClose = this.handleClose.bind(this);
         this.handleOpen = this.handleOpen.bind(this);
+        this.createCard = this.createCard.bind(this);
     }
 
     handleOpen() {
@@ -22,22 +26,43 @@ class SectionContainer extends React.Component {
 
     handleClose(child) {
         // Closes DataEntryModal 
+        var primaryText = child.state.cardText;
+        var secondaryText = child.state.cardRespText;
+        console.log(primaryText);
         this.setState({
             modalOpen: false,
-            primaryText: child.state.cardText,
-            secondaryText: child.state.cardRespText
+            primaryText: primaryText,
+            secondaryText: secondaryText 
+        }, function () {
+            this.render(); // this is probably stupid...
+        }
+        );
+    }
+
+    createCard(text) {
+        console.log('I was called');
+        var cards = this.state.cards;
+        cards.push({
+            text: text,
+            response: '',
+            id: Date.now(),
+            checked: false
         });
+        this.setState({cards: cards});
+        console.log(cards);
     }
 
     render() {
+        //<RaisedButton label='Open Modal' onTouchTap={this.handleOpen} />
         return (
             <div className='row'>
-                <div className='col-md-4 col-md-offset-4'> 
-                    <a href='#'>
+                <div className='col-md-4 col-md-offset-4 well'> 
+                    <div onClick={this.handleOpen}> 
                         <h2>{this.state.title}</h2>
-                    </a>
-                    <RaisedButton label='Open Modal' onTouchTap={this.handleOpen} />
+                    </div>
                     <DataEntryModal cardText={this.state.primaryText} cardRespText={this.state.secondaryText} open={this.state.modalOpen} title={this.state.title} closeModal={this.handleClose} />
+                    <CardDisplay cardText={this.state.primaryText} handleClick={this.handleOpen} />
+                    <AddCard handleAddCard={(card) => this.createCard(card)}/>
                 </div>
             </div>
         );
